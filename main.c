87819a9b636da487124ccce490dbe8c11e7aaee3
SadisaBalderas <a372556@alumnos.uaslp.mx>
2024-09-19 10:50:09 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
-int main(int argc, char** argv) {
-
-    initializeDataDictionary();
+int main(int argc, char** argv)
+{
+    char name[50];
+    printf("Enter a Dictionary Name: ");
+    gets(name);
+    initializeDataDictionary(name);
 
     return 0;
 }
