50e8dee2d04d1a6eb9a91e064ae582c1a53f2a07
SadisaBalderas <a372556@alumnos.uaslp.mx>
2024-09-19 10:54:17 -0600

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
     char name[50];
     printf("Enter a Dictionary Name: ");
-    gets(name);
+    fgets(name, sizeof(name), stdin);
     initializeDataDictionary(name);
 
     return 0;
