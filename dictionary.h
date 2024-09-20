0bf391ad2496813af25455acc635d256a878e583
SadisaBalderas <a372556@alumnos.uaslp.mx>
2024-09-20 10:27:50 -0600

    Update dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -2,7 +2,16 @@
 #define DICTIONARY_H
 
 #include <stdio.h>
-$include <stdlib.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+  char value[50];
+  // In-Memory Pointer
+  //struct Node *next;
+  //File pointer
+  long next; //
+}NODE;
 
 # DEFINE EMPTY_POINTER -1 
 void initializeDataDictionary();
