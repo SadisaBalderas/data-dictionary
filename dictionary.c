831ee2673ce8629c2f5bb462b6f2c2f4cfc990ec
SadisaBalderas <a372556@alumnos.uaslp.mx>
2024-09-20 10:19:09 -0600

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -3,11 +3,14 @@
 int initializeDataDictionary(const char *DictionaryName) 
 {
     long mainHeader = EMPTY_POINTER;
+    
     printf("Initializing Data Dictionary...\n");
 
     FILE *dictionary = fopen(DictionaryName, "w+");
 
     fwrite(&mainHeader, sizeof(mainHeader), 1, dictionary);
+
+    fseek(dictionary, 50, SEEK_SET);
 
     return EXIT_SUCCESS;
 }
